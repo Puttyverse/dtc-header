ee0f05c77e04cd3fcfd79a4eaa82e9238ff3dbb5
Ti0 <ti0@ti0.io>
2023-03-27 21:25:46 -0300

    Basic integration of Brand header and Custom header links

--- javascripts/discourse/initializers/discourse-custom-header-links.js
+++ javascripts/discourse/initializers/discourse-custom-header-links.js
@@ -4,7 +4,7 @@ import { wantsNewWindow } from "discourse/lib/intercept-click";
 import DiscourseURL from "discourse/lib/url";
 
 export default {
-  name: "discourse-custom-header-links",
+  name: "puttyverse-dtcz-header",
 
   initialize() {
     withPluginApi("0.8.20", (api) => {
